bb19d65a396a5542cf3fdfbd0f73c1a1b90f5050
k-e-r <damajonboua@gmail.com>
2022-05-22 02:03:31 -0700

    add user follow api

--- routes/users.js
+++ routes/users.js
@@ -49,4 +49,33 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// Follow user
+router.put('/:id/follow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $push: {
+            followers: req.body.userId,
+          },
+        });
+        await currentUser.updateOne({
+          $push: {
+            followings: req.params.id,
+          },
+        });
+        return res.status(200).json('follow succeeded');
+      } else {
+        return res.status(403).json('You have already followed this user');
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(500).json('You cannot follow yourself');
+  }
+});
+
 module.exports = router;
