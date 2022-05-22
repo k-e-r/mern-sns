b75beb02d9f6fe0a1cb5bccc7c248adc03c492e1
k-e-r <damajonboua@gmail.com>
2022-05-22 01:37:13 -0700

    add user update api

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
+const User = require('../models/User');
 
-router.get('/', (req, res) => {
-  res.send('user router');
+// CRUD
+// Update user information
+router.put('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json('Updated the information of your account');
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json('You cannot update the information of this account');
+  }
 });
 
 module.exports = router;
