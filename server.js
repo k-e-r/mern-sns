b2074d6c8d6be3cac6f3166c814f3240b9b0931a
k-e-r <damajonboua@gmail.com>
2022-05-21 18:02:54 -0700

    add endpoint

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('nodejs');
+const express = require('express');
+const app = express();
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+const PORT = 3000;
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/posts', postRoute);
+
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+// app.get('/users', (req, res) => {
+//   res.send('users');
+// });
+
+app.listen(PORT, () => console.log(`Server ${PORT}`));
