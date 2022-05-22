3788aaaae682ab93086fbc34cd92cd2f41bb1101
k-e-r <damajonboua@gmail.com>
2022-05-22 16:15:10 -0700

    add make html template

---
+++ client/src/components/topbar/Topbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function Topbar() {
+  return (
+    <div className='topbarContainer'>
+      <div className='topbarLeft'>
+        <span className='logo'>SNS</span>
+      </div>
+      <div className='topbarCenter'>
+        <div className='searchbar'>
+          <input
+            type='text'
+            className='searchInput'
+            placeholder='Please enter keyword'
+          />
+        </div>
+      </div>
+      <div className='topbarRight'>
+        <div className='topbarIconItem'>1</div>
+        <div className='topbarIconItem'>2</div>
+      </div>
+      <img src='/assets/person/1.jpg' alt='' className='topbarImg' />
+    </div>
+  );
+}
