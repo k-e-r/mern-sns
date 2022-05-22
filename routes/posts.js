73be15d7ad1f34c87bc70a6f8f87d9e78d52cb8d
k-e-r <damajonboua@gmail.com>
2022-05-22 11:01:27 -0700

    add push/pull like to a certain post api

--- routes/posts.js
+++ routes/posts.js
@@ -54,4 +54,28 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// like a post
+router.put('/:id/like', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({
+        $push: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(200).json('put like succeeded');
+    } else {
+      await post.updateOne({
+        $pull: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(200).json('pull like succeeded');
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
 module.exports = router;
