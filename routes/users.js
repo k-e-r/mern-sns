68b6b203a0554174afd3edd7231b16c269273eec
k-e-r <damajonboua@gmail.com>
2022-05-22 02:11:30 -0700

    add user unfollow api

--- routes/users.js
+++ routes/users.js
@@ -78,4 +78,33 @@ router.put('/:id/follow', async (req, res) => {
   }
 });
 
+// Unfollow user
+router.put('/:id/unfollow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({
+          $pull: {
+            followers: req.body.userId,
+          },
+        });
+        await currentUser.updateOne({
+          $pull: {
+            followings: req.params.id,
+          },
+        });
+        return res.status(200).json('unfollow succeeded');
+      } else {
+        return res.status(403).json('You cannot unfollowed this user');
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(500).json('You cannot unfollow yourself');
+  }
+});
+
 module.exports = router;
