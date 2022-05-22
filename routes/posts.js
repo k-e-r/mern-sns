692687ff761d18dee2d91d89b70e27286deee076
k-e-r <damajonboua@gmail.com>
2022-05-22 10:47:23 -0700

    add edit a post api

--- routes/posts.js
+++ routes/posts.js
@@ -12,4 +12,21 @@ router.post('/', async (req, res) => {
   }
 });
 
+// update a post
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({
+        $set: req.body,
+      });
+      return res.status(200).json('Updated succeeded');
+    } else {
+      return res.status(403).json("You cannot edit other person's post");
+    }
+  } catch (err) {
+    return res.status(403).json(err);
+  }
+});
+
 module.exports = router;
