12616a9387a30d62048ecddcf38f1727b0a7cd8d
k-e-r <damajonboua@gmail.com>
2022-05-21 19:47:06 -0700

    add connect with MongoDB and Node.js

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
 const PORT = 3000;
+const mongoose = require('mongoose');
+const connectionURL = process.env.MONGO_URL;
+
+mongoose
+  .connect(connectionURL)
+  .then(() => {
+    console.log('Connecting with DB...');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
