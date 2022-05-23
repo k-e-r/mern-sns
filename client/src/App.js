1e06ac7250949003f9799ecb43c1ec7bdb1b8207
k-e-r <damajonboua@gmail.com>
2022-05-22 21:57:07 -0700

    add make register component

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Profile from './pages/profile/Profile';
+import Register from './pages/register/Register';
 
 function App() {
-  return <Login />;
+  return <Register />;
 }
 
 export default App;
