81f02278f1319485149fdcec4907e91d66f70f1b
k-e-r <damajonboua@gmail.com>
2022-05-22 22:17:29 -0700

    modify port

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
-const PORT = 3000;
+const PORT = 5000;
 const mongoose = require('mongoose');
 const connectionURL = process.env.MONGO_URL;
 
