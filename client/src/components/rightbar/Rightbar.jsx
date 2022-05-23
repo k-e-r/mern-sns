1079aa5612ec9532caaec7a3cc3d98a36c067f54
k-e-r <damajonboua@gmail.com>
2022-05-22 18:48:44 -0700

    add rightbar css

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -9,15 +9,30 @@ export default function Rightbar() {
           <img src='/assets/star.png' alt='' className='starImg' />
           <span className='eventText'>Event holding!</span>
         </div>
-        <img src='/assets/event.jpeg' alt='' />
+        <img src='/assets/event.jpeg' alt='' className='eventImg' />
         <h4 className='rightbarTitle'>online friends</h4>
-        <ul className='rightbarFriend'>
+        <ul className='rightbarFriendList'>
           <li className='rightbarFriend'>
             <div className='rightbarProfileImgContainer'>
-              <img src='/assets/person/1.jpeg' alt='' />
+              <img
+                src='/assets/person/1.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
               <span className='rightbarOnline'></span>
             </div>
             <span className='rightbarUsername'>Shin</span>
+          </li>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img
+                src='/assets/person/2.jpeg'
+                alt=''
+                className='rightbarProfileImg'
+              />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Tanaka</span>
           </li>
         </ul>
       </div>
