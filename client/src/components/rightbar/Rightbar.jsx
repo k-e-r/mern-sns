967c7ec1aeb577f7d139fe68e53047450653cb51
k-e-r <damajonboua@gmail.com>
2022-05-22 18:42:42 -0700

    add make rightbar component

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -2,5 +2,25 @@ import React from 'react';
 import './Rightbar.css';
 
 export default function Rightbar() {
-  return <div className='rightbar'>Rightbar</div>;
+  return (
+    <div className='rightbar'>
+      <div className='rightbarWrapper'>
+        <div className='eventContainer'>
+          <img src='/assets/star.png' alt='' className='starImg' />
+          <span className='eventText'>Event holding!</span>
+        </div>
+        <img src='/assets/event.jpeg' alt='' />
+        <h4 className='rightbarTitle'>online friends</h4>
+        <ul className='rightbarFriend'>
+          <li className='rightbarFriend'>
+            <div className='rightbarProfileImgContainer'>
+              <img src='/assets/person/1.jpeg' alt='' />
+              <span className='rightbarOnline'></span>
+            </div>
+            <span className='rightbarUsername'>Shin</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
